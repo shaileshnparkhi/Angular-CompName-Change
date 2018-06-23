88592bf7946708df10d3b5e6f87d9d518b769747
Shaileshnparkhi <shaileshnparkhi@gmail.com>
2018-06-23 10:31:31 +0530

    initialial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MarvellousInfosystems } from './app.component';
+
+@NgModule({
+  declarations: [
+    MarvellousInfosystems
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [MarvellousInfosystems]
+})
+export class AppModule { }
